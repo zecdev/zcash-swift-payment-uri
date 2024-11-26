8ddbbc5c1072d7a48690b21345c0dfccb56744a3
Francisco Gindre <francisco.gindre@gmail.com>
2024-11-26 16:06:49 -0300

    Add a test that uses the whole Base64URL character set

--- Tests/ZcashPaymentURITests/MemoBytesTests.swift
+++ Tests/ZcashPaymentURITests/MemoBytesTests.swift
@@ -38,6 +38,22 @@ final class MemoBytesTests: XCTestCase {
         XCTAssertEqual(memo.toBase64URL(), expectedBase64)
     }
 
+    /// Cross-check using all Base64URL characters and do a round-trip
+    func testRoundTripWithAllBase64URLCharacters() throws {
+        let base64URLCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+        let e: [UInt8] = [
+            0x00, 0x10, 0x83, 0x10, 0x51, 0x87, 0x20, 0x92, 0x8b, 0x30, 0xd3, 0x8f, 0x41, 0x14, 0x93, 0x51,
+            0x55, 0x97, 0x61, 0x96, 0x9b, 0x71, 0xd7, 0x9f, 0x82, 0x18, 0xa3, 0x92, 0x59, 0xa7, 0xa2, 0x9a,
+            0xab, 0xb2, 0xdb, 0xaf, 0xc3, 0x1c, 0xb3, 0xd3, 0x5d, 0xb7, 0xe3, 0x9e, 0xbb, 0xf3, 0xdf, 0xbf
+        ]
+
+        let memo = try MemoBytes(base64URL: base64URLCharacters)
+        let memoFromBytes = try MemoBytes(bytes: e)
+
+        XCTAssertEqual(memo, memoFromBytes)
+        XCTAssertEqual(memo.toBase64URL(), base64URLCharacters)
+    }
+
     func testUnicodeMemo() throws {
         let memoUTF8Text = "This is a unicode memo ✨🦄🏆🎉"
         let expectedBase64 = "VGhpcyBpcyBhIHVuaWNvZGUgbWVtbyDinKjwn6aE8J-PhvCfjok"
